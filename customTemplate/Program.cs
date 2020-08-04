5895f79aedb3f347d01055582192444c4312f32b
Robin Mauritz <robinmauritzrm@gmail.com>
2020-08-04 18:17:34 +0700

    Add new custom dotnet template

---
+++ customTemplate/Program.cs
@@ -0,0 +1,37 @@
+﻿/*
+ * Copyright (C) 2020  Robin Mauritz Languju
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+//* Repository link: https://github.com/RoganMatrivski/Coding.ID-Code-Repository
+
+using System;
+
+namespace customTemplate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+BEGIN:
+
+            //TODO: Add code function here
+
+            Console.ReadKey(true);
+            Console.Clear();
+goto BEGIN;
+        }
+    }
+}
