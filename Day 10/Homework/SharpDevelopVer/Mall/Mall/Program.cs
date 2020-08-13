b5d0889aeec1f3e031262917d12413320e097850
Robin Mauritz <robinmauritzrm@gmail.com>
2020-08-13 20:34:11 +0700

    Add day 10 homework and tasks - SharpDevelopVer

---
+++ Day 10/Homework/SharpDevelopVer/Mall/Mall/Program.cs
@@ -0,0 +1,190 @@
+/*
+ * Created by SharpDevelop.
+ * User: NAWADATA
+ * Date: 28/03/2020
+ * Time: 19:46
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+
+namespace Mall
+{
+    public class Mall
+    {
+        private List<Store> listStore;
+        private string name;
+
+        public Mall(string param_name)
+        {
+            //TODO: Add code
+            this.listStore = new List<Store>();
+            this.name = param_name;
+        }
+
+        public List<Store> ListStore
+        {
+            //TODO: Add code
+            set { listStore = value; }
+            get { return listStore; }
+        }
+
+        public string Name
+        {
+            set { name = value; }
+            get { return name; }
+        }
+
+    }
+
+    public class Store
+    {
+        private List<Smartphone> listPhone;
+        private string name;
+
+        public Store(string param_name)
+        {
+            listPhone = new List<Smartphone>();
+            name = param_name;
+        }
+
+        public List<Smartphone> ListPhone
+        {
+            set { listPhone = value; }
+            get { return listPhone; }
+        }
+
+        public string Name
+        {
+            set { name = value; }
+            get { return name; }
+        }
+
+    }
+
+    public class Smartphone
+    {
+        private string name;
+        private int rom;
+        private int ram;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public int Rom
+        {
+            get { return rom; }
+            set { rom = value; }
+        }
+        public int Ram
+        {
+            get { return ram; }
+            set { ram = value; }
+        }
+        public int Calculate()
+        {
+            int price = (100000 * Rom) + (150000 * Ram);
+            return price;
+        }
+    }
+
+    class Program
+    {
+        public static Store insertStoreData()
+        {
+            Console.Write("What is Store Name ? ");
+            string store_name = Console.ReadLine();
+            Store objStore = new Store(store_name);
+
+            //TODO: Add code
+            Console.Write("How many smartphones? ");
+            int phoneCount = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < phoneCount; i++)
+            {
+                Smartphone newSmartphone = new Smartphone();
+
+                Console.Write("Smartphone name #{0}: ", i + 1);
+                newSmartphone.Name = Console.ReadLine();
+                Console.Write("Smartphone ROM: ");
+                newSmartphone.Rom = int.Parse(Console.ReadLine());
+                Console.Write("Smartphone RAM: ");
+                newSmartphone.Ram = int.Parse(Console.ReadLine());
+
+                objStore.ListPhone.Add(newSmartphone);
+            }
+
+            return objStore;
+        }
+
+        public static Mall insertMallData()
+        {
+            Console.Write("What is Mall Name ? \n");
+            string mal_name = Console.ReadLine();
+            Mall objMall = new Mall(mal_name);
+
+            //TODO: Add code
+            Console.Write("How many Store for {0}? ", mal_name);
+            int storeCount = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < storeCount; i++)
+            {
+                Store newStore = insertStoreData();
+
+                objMall.ListStore.Add(newStore);
+            }
+
+            return objMall;
+        }
+
+        public static void Main(string[] args)
+        {
+        Begin:
+            Console.Write("How many Mall ? ");
+            string jml_mall = Console.ReadLine();
+
+            List<Mall> listMall = new List<Mall>();
+
+            for (int y = 0; y < Convert.ToInt16(jml_mall); y++)
+            {
+                //TODO: Add code
+                //Insert Mall to listMall
+                listMall.Add(insertMallData());
+            }
+
+            foreach (Mall mall in listMall)
+            {
+                //TODO: Add code
+                // Display List Store and Smartphone from each Store 
+                Console.WriteLine("This is List Store from Mall {0}", mall.Name);
+                foreach (Store store in mall.ListStore)
+                {
+                    Console.WriteLine("This is List Stock from Store {0}", store.Name);
+                    foreach (Smartphone phone in store.ListPhone)
+                    {
+                        Console.WriteLine("The Price of Smartphone {0} with ROM {1} GB and RAM {2} GB is Rp {3}", phone.Name, phone.Rom, phone.Ram, phone.Calculate());
+                    }
+                }
+
+                //TODO: Add code
+                //4. Display List Stock from each Store
+                /*
+                    Format
+                    This is List Stock from [Store Name]
+
+                    The Price of Smartphone [Smartphone Name] with ROM [Rom]GB and RAM [Ram]GB is Rp [Harga]
+                    The Price of Smartphone [Smartphone Name] with ROM [Rom]GB and RAM [Ram]GB is Rp [Harga]
+                */
+
+            }
+
+            Console.WriteLine();
+            Console.Write("Press any key to continue . . . ");
+            Console.ReadKey(true);
+            Console.Clear();
+            goto Begin;
+        }
+    }
+}
---
+++ Day 10/Homework/SharpDevelopVer/Race/Race/Program.cs
@@ -0,0 +1,159 @@
+/*
+ * Created by SharpDevelop.
+ * User: NAWADATA
+ * Date: 29/03/2020
+ * Time: 18:25
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+
+namespace Race
+{
+    public class Car
+    {
+        private string name;
+        private int speed;
+        public Car(int param_speed, string param_name)
+        {
+            //TODO: Add code here
+            //your code here
+            //set speed and name
+            this.speed = param_speed;
+            this.name = param_name;
+
+        }
+
+        public int Speed
+        {
+            get
+            {
+                return speed;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+        }
+    }
+
+    public class Race
+    {
+        private string name;
+        private int length;
+        private List<Car> listCar;
+
+        public List<Car> ListCar
+        {
+            get
+            {
+                return listCar;
+            }
+        }
+
+        public Race(string param_name, int param_length)
+        {
+            //TODO: Add code here
+            //your code here
+            //set name, length, and initiate list
+            this.name = param_name;
+            this.length = param_length;
+            this.listCar = new List<Car>();
+        }
+
+        public void proRace()
+        {
+            Dictionary<string, int> carPosition = new Dictionary<string, int>(this.listCar.Count);
+            for (int x = 1; true; x++)
+            {
+                Console.WriteLine("Current Position for Loop " + x);
+                //TODO: Add code here
+                //your code here
+                //loop per car and show current position for each car
+
+                bool raceEnd = false;
+
+                foreach (var car in this.listCar)
+                {
+                    if (carPosition.ContainsKey(car.Name))
+                    {
+                        carPosition[car.Name] += car.Speed;
+                    }
+                    else
+                    {
+                        carPosition.Add(car.Name, car.Speed);
+                    }
+
+                    Console.Write("Current position for car {0} is {1}\n", car.Name, carPosition[car.Name]);
+
+                    if (carPosition[car.Name] > this.length)
+                    {
+                        Console.WriteLine("The winner is car {0}", car.Name);
+                        raceEnd = true;
+                        break;
+                    }
+                }
+
+                if (raceEnd)
+                {
+                    break;
+                }
+            }
+
+        }
+    }
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+        Begin:
+
+            Console.Write("Race Name? ");
+            string race_name = Console.ReadLine();
+
+            Console.Write("Length for " + race_name + "? ");
+            int race_length = Convert.ToInt16(Console.ReadLine());
+
+            //TODO: Add code here
+            //your code here
+            //create object of Race
+            Race objRace = new Race(race_name, race_length);
+
+            //TODO: Add code here
+            //your code here
+            //input cars
+
+            Console.Write("How many cars? ");
+            int carCount = Convert.ToInt16(Console.ReadLine());
+
+            for (int i = 0; i < carCount; i++)
+            {
+                Console.Write("Car Name? ");
+                string carName = Console.ReadLine();
+
+                Console.Write("Speed for Car " + carName + "? ");
+                int carSpeed = Convert.ToInt16(Console.ReadLine());
+
+                objRace.ListCar.Add(new Car(carSpeed, carName));
+            }
+
+            Console.WriteLine();
+
+            //TODO: Add code here
+            //your code here
+            //Show race result
+            objRace.proRace();
+
+            Console.Write("Press any key to continue . . . ");
+            Console.ReadKey(true);
+            Console.Clear();
+            goto Begin;
+        }
+    }
+}
